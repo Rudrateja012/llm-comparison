aba456e7664f29c5fb59d8eb7fbfc9a6a70bf8ba
KOURU RUDRA TEJA <rudrateja012@gmail.com>
2026-01-03 14:18:34 +0530

    day9

--- utils/report.py
+++ utils/report.py
@@ -4,4 +4,12 @@ from datetime import datetime
 
 def generate_report(prompt:str,response:dict):
     os.makedirs("data/completions_reports", exist_ok=True)
-    
+
+    row = []
+    for model,output in response.items():
+        row.append({
+            "model": model,
+            "prompt": prompt,
+            "Response": output,
+            "Timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        })  
