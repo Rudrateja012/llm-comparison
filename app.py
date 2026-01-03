12def6ff1f67f9395079c0d31526a19ef58a5b44
KOURU RUDRA TEJA <rudrateja012@gmail.com>
2026-01-03 14:47:05 +0530

    day

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ from utils.report import generate_report
 
 st.set_page_config(
     page_title="LLM Comparison Tool",
-    page_icon="🤖",
-    
+    page_icon="🤖👾👽",
+    layout="wide",
+)
+st.title("🤖👾👽 LLM Comparison Tool")
+st.markdown(
+    """
+    compare **chatGPT**, **GPT-4**, **Claude**, **Gemini** and more models side by side.
+    """
+)
+prompt = st.text_area(
+    "Enter your prompt here:", 
+    height=200
+    placeholder="e.g., Write a poem about the sea in the style of Shakespeare."
+)
